7f5a224131765af786822b891c601c04193b5c37
PeterFred <32250775+PeterFred@users.noreply.github.com>
2019-11-10 07:46:02 +1300

    Controller and customer get methods created

---
+++ BikeStoreProject/Controllers/BikeStoreController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BikeStoreDataAccess;
+
+namespace BikeStoreProject.Controllers
+{
+    public class BikeStoreController : ApiController
+    {
+        public IEnumerable<customers> Get()
+        {
+            using (BikeStoresEntities entities = new BikeStoresEntities())
+            {
+                return entities.customers.ToList();
+            }
+        }
+
+        public customers Get(int id)
+        {
+            using(BikeStoresEntities entities = new BikeStoresEntities())
+            {
+                return entities.customers.FirstOrDefault(e => e.customer_id == id);
+            }
+        }
+    }
+}
